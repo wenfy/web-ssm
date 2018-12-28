2d6a9c02b78110444525d0a34f42724a9b70e646
文飞扬 <wenfeiyang@yixinchina.com>
2018-12-28 17:08:45 +0800

    test01

---
+++ src/main/java/com/heitian/ssm/controller/test01.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2014-2020 Nuhtech Technology(Beijing) Co.,Ltd.
+ *
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary
+ * information of Nuhtech Company of China.
+ * ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only
+ * in accordance with the terms of the contract agreement
+ * you entered into with Nuhtech inc.
+ *
+ */
+
+package com.heitian.ssm.controller;
+
+/**
+ * @author wfy
+ * @date 2018/12/28
+ */
+public class test01 {
+}
